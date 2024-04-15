49c9ee4efe28992b9f6ef5e92057386432517c14
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2024-04-15 22:29:01 +0200

    pitanja se sada brisu iz liste kada se izaberu ali dugme za refresh ne radi

--- src/components/Naslov.jsx
+++ src/components/Naslov.jsx
@@ -1,9 +1,21 @@
-import classes from './Naslov.module.css'
+import classes from "./Naslov.module.css";
+import { BsFillPencilFill } from "react-icons/bs";
+import { FiRefreshCcw } from "react-icons/fi";
 
-function Naslov() {
-    return (
-        <h1 className={classes.naslov}>Exercises for syntax</h1>
-    )
+function Naslov({refresh}) {
+  return (
+    <header>
+      <h1 className={classes.naslov}>
+        <BsFillPencilFill />
+        Exercises for syntax
+      </h1>
+
+      <button className={classes.dugme} onClick={refresh} type="button">
+        <FiRefreshCcw size={16} />
+        Refresh Questions
+      </button>
+    </header>
+  );
 }
 
 export default Naslov;
--- src/components/Pitanje.jsx
+++ src/components/Pitanje.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import { RiSpeakFill } from "react-icons/ri";
 import classes from "./Pitanje.module.css";
 
 function Pitanje({ svaPitanja, imePitanja }) {
   const [novoPitanje, setNovoPitanje] = useState("Press the button");
+  let pitanjaArray = [];
+  pitanjaArray = svaPitanja;
 
   function getPitanjeHandler(event) {
-    setNovoPitanje(svaPitanja[Math.floor(Math.random() * svaPitanja.length)]);
+    console.log(pitanjaArray);
+    const index = Math.floor(Math.random() * pitanjaArray.length);
+    setNovoPitanje(pitanjaArray[index]);
+    pitanjaArray.splice(index, 1);
+  }
+
+  function refreshHandler(event) {
+    pitanjaArray = svaPitanja;
   }
 
   return (
@@ -13,9 +23,10 @@ function Pitanje({ svaPitanja, imePitanja }) {
       <p className={classes.imePitanja}>{imePitanja}</p>
       <li className={classes.container}>
         <button className={classes.dugme} onClick={getPitanjeHandler}>
+          <RiSpeakFill />
           Ask Me
         </button>
-        <p className={classes.pitanje}>{novoPitanje}</p>
+        <p className={classes.pitanje}>{pitanjaArray.length != 0 ? novoPitanje : "All questions asked, please refresh them"}</p>
       </li>
     </>
   );
