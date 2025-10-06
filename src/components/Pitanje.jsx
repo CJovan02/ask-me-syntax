7adcb31b6aad12c05395b25890e2189778209694
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-10-06 16:27:12 +0200

    Revert "added question counter"
    This reverts commit a06c67229d542057ee2ed7536f654c956fdb8bc4.

--- src/components/Pitanje.jsx
+++ src/components/Pitanje.jsx
@@ -32,26 +32,20 @@ function Pitanje({ svaPitanja, imePitanja }) {
     <>
       <p className={classes.imePitanja}>{imePitanja}</p>
       <li className={classes.container}>
-        <div className={classes.leftGroup}>
-          {!refresh ? (
-            <button className={classes.dugme} onClick={getPitanjeHandler}>
-              <RiSpeakFill size={20} />
-              Ask Me
-            </button>
-          ) : (
-            <button className={classes.refresh} onClick={refreshHandler}>
-              <FiRefreshCcw size={18} />
-              Refresh
-            </button>
-          )}
-          <p className={classes.pitanje}>
-            {!refresh
-              ? novoPitanje
-              : "All questions asked, please refresh them"}
-          </p>
-        </div>
-
-        <p className={classes.counter}>{pitanjaArray.length} left</p>
+        {!refresh ? (
+          <button className={classes.dugme} onClick={getPitanjeHandler}>
+            <RiSpeakFill size={20} />
+            Ask Me
+          </button>
+        ) : (
+          <button className={classes.refresh} onClick={refreshHandler}>
+            <FiRefreshCcw size={18} />
+            Refresh
+          </button>
+        )}
+        <p className={classes.pitanje}>
+          {!refresh ? novoPitanje : "All questions asked, please refresh them"}
+        </p>
       </li>
     </>
   );
