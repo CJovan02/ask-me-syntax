b1aa706d48429ef11b6f1970c01c81b8930d8381
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-10-06 16:57:33 +0200

    added vercel env

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ask-me-syntax/"
+  //base: "/ask-me-syntax/"
+  base: process.env.VITE_BASE_PATH || "/ask-me-syntax"
 })
